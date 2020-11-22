d974cdd3f0b156773c275329fa8bbed9bb696a4b
Ciaran McNally <mak@redbrick.dcu.ie>
2020-11-22 13:42:53 +0100

    Rust embedded in python

---
+++ embeddedpy/target/release/deps/embed.d
@@ -0,0 +1,5 @@
+/root/learning-rust-syntax/embeddedpy/target/release/deps/libembed.so: src/lib.rs
+
+/root/learning-rust-syntax/embeddedpy/target/release/deps/embed.d: src/lib.rs
+
+src/lib.rs:
---
+++ embeddedpy/target/release/libembed.d
@@ -0,0 +1,1 @@
+/root/learning-rust-syntax/embeddedpy/target/release/libembed.so: /root/learning-rust-syntax/embeddedpy/src/lib.rs
