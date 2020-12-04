024efc75be62c10d1b7214f08809338f122a781f
Ciaran McNally <mak@redbrick.dcu.ie>
2020-12-04 09:05:04 -0500

    AdventOfCode2020

--- AdventOfCode2020/day1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-/root/learning-rust-syntax/AdventOfCode2020/day1/target/debug/day1: /root/learning-rust-syntax/AdventOfCode2020/day1/src/main.rs
--- AdventOfCode2020/day1/target/debug/deps/day1-5b8a2572d1cf922f.d
+++
@@ -1,5 +0,0 @@
-/root/learning-rust-syntax/AdventOfCode2020/day1/target/debug/deps/day1-5b8a2572d1cf922f: src/main.rs
-
-/root/learning-rust-syntax/AdventOfCode2020/day1/target/debug/deps/day1-5b8a2572d1cf922f.d: src/main.rs
-
-src/main.rs:
